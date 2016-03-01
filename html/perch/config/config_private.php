93d8ef2f9f376afd308a0ff107c1fa216217f066
Darryl Hein <dhein@xmmedia.com>
2016-03-01 13:38:17 -0700

    double to single quotes

--- html/perch/config/config_private.php
+++ html/perch/config/config_private.php
@@ -2,8 +2,8 @@
     // @todo-perch
     define('PERCH_DB_SERVER', 'localhost');
     define('PERCH_DB_DATABASE', '');
-    define("PERCH_DB_USERNAME", '');
-    define("PERCH_DB_PASSWORD", '');
+    define('PERCH_DB_USERNAME', '');
+    define('PERCH_DB_PASSWORD', '');
 
     define('PERCH_EMAIL_USERNAME', '');
     define('PERCH_EMAIL_PASSWORD', '');
