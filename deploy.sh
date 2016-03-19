23a4dadfc20254ebdc0a40d5a51a01e3bdd13248
Darryl Hein <dhein@xmmedia.com>
2016-03-19 12:55:06 -0600

    tweaking deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 echo "-- Deploy Setup"
 # this will setup nvm so the rest will work
 . ~/.nvm/nvm.sh
-. ./node_setup.sh
 
-# install and run gulp
+# build commands
+. ./node_setup.sh || exit 1
 echo "-- npm install"
 npm install || exit 1
 echo "-- Run Gulp"
