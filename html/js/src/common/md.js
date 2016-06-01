456f0dce27cd9da37faeb6993b56fb2a58e573f2
Darryl Hein <dhein@xmmedia.com>
2016-06-01 11:26:43 -0600

    fixing the md JS

--- html/js/src/common/md.js
+++ html/js/src/common/md.js
@@ -14,32 +14,32 @@ var md = {
         return $('.js-md-modal');
     },
     open : function() {
-        admin_md.get_md().addClass('md-modal-show');
+        md.get_md().addClass('md-modal-show');
 
-        admin_md.add_scroll();
+        md.add_scroll();
 
         $(document).one('keyup', function(e) {
             if (e.keyCode == 27) {
-                admin_md.close();
+                md.close();
             }
         });
         $(window).on('resize', function() {
             if (this.md_resize) clearTimeout(this.md_resize);
             this.md_resize = setTimeout(function() {
-                admin_md.add_scroll();
+                md.add_scroll();
             }, 500);
         });
     },
     close : function() {
-        admin_md.get_md().removeClass('md-modal-show md-scroll')
+        md.get_md().removeClass('md-modal-show md-scroll')
             .find('.js-md-content_wrap').html('');
     },
     add_scroll : function() {
-        var md = admin_md.get_md();
-        if ($('.js-md-content_wrap').height() > md.height()) {
-            md.addClass('md-modal-scroll');
+        var md_wrap = md.get_md();
+        if ($('.js-md-content_wrap').height() > md_wrap.height()) {
+            md_wrap.addClass('md-modal-scroll');
         } else {
-            md.removeClass('md-modal-scroll');
+            md_wrap.removeClass('md-modal-scroll');
         }
     }
 };
