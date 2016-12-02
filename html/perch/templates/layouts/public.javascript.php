84b246a3cd5a2a376ca5edc8d059f0c065e4ecf8
Darryl Hein <dhein@xmmedia.com>
2016-12-02 11:38:39 -0700

    making JS async

--- html/perch/templates/layouts/public.javascript.php
+++ html/perch/templates/layouts/public.javascript.php
@@ -1,4 +1,4 @@
-<script src="<?php Common::version_file('/js/public.min.js'); ?>"></script>
+<script src="<?php Common::version_file('/js/public.min.js'); ?>" async></script>
 
 <?php perch_layout('global.analytics', [
     'analytics_id' => GOOGLE_ANALYTICS_ID,
