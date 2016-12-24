062bf3e7cf47daa1974cf307bbb7c35f0cd5f34e
Darryl Hein <dhein@xmmedia.com>
2016-12-23 21:58:32 -0700

    changing JS scripts to defer from async
    based on http://calendar.perfplanet.com/2016/prefer-defer-over-async/

--- html/perch/templates/layouts/public.javascript.php
+++ html/perch/templates/layouts/public.javascript.php
@@ -1,4 +1,4 @@
-<script src="<?php Common::version_file('/js/public.min.js'); ?>" async></script>
+<script src="<?php Common::version_file('/js/public.min.js'); ?>" defer></script>
 
 <?php perch_layout('global.analytics', [
     'analytics_id' => GOOGLE_ANALYTICS_ID,
