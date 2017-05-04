e1f2f4adc8ca6927d676ecad27203147573dd8d2
Darryl Hein <dhein@xmmedia.com>
2017-05-04 11:15:44 -0600

    disable vue production tip

--- html/js/src/public.js
+++ html/js/src/public.js
@@ -7,6 +7,9 @@ import svg_icons from './common/svg_icons.vue';
 // SASS/CSS
 import '../../css/sass/public.scss';
 
+// disable the warning about dev/prod
+Vue.config.productionTip = false;
+
 new Vue({
     el: '#app',
     components: {
