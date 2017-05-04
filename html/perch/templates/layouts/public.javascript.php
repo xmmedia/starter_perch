c391a569aea9f58b31609cd9bd92d449ae0eeb9b
Darryl Hein <dhein@xmmedia.com>
2017-05-04 11:14:47 -0600

    removing .min on JS files

--- html/perch/templates/layouts/public.javascript.php
+++ html/perch/templates/layouts/public.javascript.php
@@ -1,4 +1,4 @@
-<script src="<?php Common::version_file('/js/public.min.js'); ?>" defer></script>
+<script src="<?php Common::version_file('/js/public.js'); ?>" defer></script>
 
 <?php perch_layout('global.analytics', [
     'analytics_id' => GOOGLE_ANALYTICS_ID,
