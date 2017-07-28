220d9907dd694a821956a42cb416c46508f262bb
Darryl Hein <dhein@xmmedia.com>
2017-07-28 13:02:11 -0500

    updating node setup

--- node_setup.sh
+++ node_setup.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
 echo "-- Setting up node"
+
 nvm install
 nvm use --delete-prefix --silent
-if ! [ -x "$(command -v webpack)" ]; then
-    npm install webpack -g
-fi
 
 # install local node packages with yarn
 yarn
-
-# not sure this is needed, but appears yarn doesn't properly install node-sass
-echo "-- Rebuild node-sass"
-npm rebuild node-sass --silent
 
 echo "-- node Setup Done"
