329befecabc782c7bec2ac16cc1d187ce5662712
Darryl Hein <dhein@xmmedia.com>
2018-09-11 10:33:14 -0600

    make browsehappy.com link https

--- html/perch/templates/layouts/public.ie9_warning.php
+++ html/perch/templates/layouts/public.ie9_warning.php
@@ -3,7 +3,7 @@
     <div class="max-w-md my-0 mx-auto p-3 text-white text-center">
         <strong class="text-xl">This site does not support the browser you are using.</strong><br>
         We recommend upgrading your browser as it's out-of-date.<br>
-        Please check <a href="http://browsehappy.com/" class="text-inherit hover:text-grey-dark">these instructions regarding updating your browser</a>.<br>
+        Please check <a href="https://browsehappy.com/" class="text-inherit hover:text-grey-dark">these instructions regarding updating your browser</a>.<br>
         We only support Internet Explorer version 9 and newer or any evergreen browser (ie, Chrome, Safari, Edge, Firefox).
     </div>
 </div>
