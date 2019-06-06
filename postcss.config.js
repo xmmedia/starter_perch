7d3d656ffec7b80b1cf195464f2588c634ae5d57
Herman Steuernagel <herman@thefourth.ca>
2019-06-06 07:11:18 -0700

    updating to tailwind 1.0

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     plugins: [
-        require('tailwindcss')('./tailwind.js'),
+        require('tailwindcss'),
         require('autoprefixer'),
     ],
     sourceMap : 'prev',
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  theme: {
+
+    extend: {
+        colors: {
+            'inherit': 'inherit',
+            'orange-600': '#de751f',
+        },
+        borderWidth: {
+            '10': '10px',
+        },
+        maxWidth: {
+            '1/2': '50%',
+            '11/12': '91%',
+        },
+        screens: {
+            'xs': '400px',
+            'max': '1245px',
+        },
+    },
+  },
+  variants: {},
+  plugins: []
+}
