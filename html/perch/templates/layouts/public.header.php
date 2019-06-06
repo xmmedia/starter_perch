008b3079c1bea7b261d0b9fd5bb5d96e3d301212
Herman Steuernagel <herman@thefourth.ca>
2019-06-06 08:51:57 -0700

    adjusting classes

--- html/perch/templates/layouts/public.header.php
+++ html/perch/templates/layouts/public.header.php
@@ -1,9 +1,9 @@
-<header class="max-w-3xl">
+<header class="max-w-6xl">
     <!-- @todo-perch -->
     Company Name
 
     <nav>
-        <ul class="list-reset">
+        <ul class="">
             <li class="header-nav_item"><a href="/">Home</a></li>
         </ul>
     </nav>
