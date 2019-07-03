fd7cedb5c5711d8de63c3ac01317dc37a2490f52
Herman Steuernagel <herman@thefourth.ca>
2019-07-03 09:09:46 -0700

    fixing alert width

--- html/perch/templates/layouts/public.ie9_warning.php
+++ html/perch/templates/layouts/public.ie9_warning.php
@@ -1,6 +1,6 @@
 <!--[if lte IE 9]>
 <div class="bg-red-500">
-    <div class="max-w-6xl my-0 mx-auto p-3 text-white text-center">
+    <div class="max-w-lg my-0 mx-auto p-3 text-white text-center">
         <strong class="text-xl">This site does not support the browser you are using.</strong><br>
         We recommend upgrading your browser as it's out-of-date.<br>
         Please check <a href="https://browsehappy.com/" class="text-inherit hover:text-grey-600">these instructions regarding updating your browser</a>.<br>
