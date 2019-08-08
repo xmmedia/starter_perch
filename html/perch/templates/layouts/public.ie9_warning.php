e859991e6dbd91ca6e8a0a77094491d57a4d64a3
Darryl Hein <dhein@xmmedia.com>
2019-08-07 21:20:16 -0600

    tweak IE warning

--- html/perch/templates/layouts/public.ie9_warning.php
+++ html/perch/templates/layouts/public.ie9_warning.php
@@ -4,7 +4,7 @@
         <strong class="text-xl">This site does not support the browser you are using.</strong><br>
         We recommend upgrading your browser as it's out-of-date.<br>
         Please check <a href="https://browsehappy.com/" class="text-inherit hover:text-grey-600">these instructions regarding updating your browser</a>.<br>
-        We only support Internet Explorer version 9 and newer or any evergreen browser (ie, Chrome, Safari, Edge, Firefox).
+        We only support evergreen browsers, such as Chrome, Safari, Edge, Firefox.
     </div>
 </div>
 <![endif]-->
