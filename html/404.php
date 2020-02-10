c28acd4e9a640bb66f1831012e771b22d3d01f37
Herman Steuernagel <herman@thefourth.ca>
2020-02-10 09:04:09 -0800

    updating 404 layout

--- html/404.php
+++ html/404.php
@@ -8,16 +8,13 @@
 <body>
     <?php perch_layout('public.header'); ?>
 
-    <div class="main_content">
-        <div class="grid">
-            <div class="col">
-                <h1>Oops! An Error Occurred</h1>
+    <div class="w-content-copy">
+        <h1>Oops! An Error Occurred</h1>
 
-                <h2>The server returned a "404 Not Found".</h2>
-                <p>Something is broken. Please let us know what you were doing when this error occurred. We will fix it as soon as possible. Sorry for any inconvenience caused.</p>
-            </div>
-        </div>
+        <h2>The server returned a "404 Not Found".</h2>
+        <p>Something is broken. Please let us know what you were doing when this error occurred. We will fix it as soon as possible. Sorry for any inconvenience caused.</p>
     </div>
+
 
     <?php perch_layout('public.footer'); ?>
 
