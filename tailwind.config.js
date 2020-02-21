b75b0b61aa402110aec0739693c0ca5ab0b49503
Herman Steuernagel <herman@thefourth.ca>
2020-02-21 15:08:07 -0800

    adding heading fonts and variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,39 @@
 module.exports = {
-  theme: {
-    extend: {
-        colors: {
-            'inherit': 'inherit',
-        },
-        borderWidth: {
-            '10': '10px',
-        },
-        maxWidth: {
-            '1/2': '50%',
-            '11/12': '91%',
-        },
-        screens: {
-            'xs': '400px',
-            'max': '1245px',
+    theme: {
+        extend: {
+            colors: {
+                'inherit': 'inherit',
+            },
+            borderWidth: {
+                '10': '10px',
+            },
+            maxWidth: {
+                '1/2': '50%',
+                '11/12': '91%',
+            },
+            screens: {
+                'xs': '400px',
+                'max': '1245px',
+                'print': { 'raw': 'print' },
+                'retina': { 'raw': '(-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi)' },
+            },
+            fontFamily: {
+                'headings': [
+                    '"Helvetica Neue"',
+                    'Arial',
+                    'sans-serif',
+                    '"Apple Color Emoji"',
+                    '"Segoe UI Emoji"',
+                    '"Segoe UI Symbol"',
+                    '"Noto Color Emoji"',
+                ],
+            },
         },
     },
-  },
-  variants: {
-    textColor: ['group-hover'],
-    borderColor: ['group-hover'],
-    opacity: ['responsive', 'hover', 'focus'],
-  },
-  plugins: []
+    variants: {
+        textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+        borderColor: ['responsive', 'hover', 'focus', 'group-hover'],
+        opacity: ['responsive', 'hover', 'focus', 'group-hover'],
+    },
+    plugins: []
 }
