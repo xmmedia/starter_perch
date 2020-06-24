e780b542b8ad76e84da9fc44b0a4e9c890bcf439
Herman Steuernagel <herman@thefourth.ca>
2020-06-23 18:01:25 -0700

    purge css to tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,34 +1,7 @@
-// postcss.config.js
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    // Specify the paths to all of the template files in your project & any other modules containing CSS class names
-    content: [
-        'html/**/*.php',
-        'html/perch/templates/**/*.html',
-        'html/perch/templates/**/*.php',
-        'html/perch/addons/**/*.html',
-        'html/perch/addons/**/*.php',
-        'html/js/src/**/*.vue',
-        'html/js/src/**/*.js',
-        'node_modules/vue-js-modal/dist/index.js',
-        // 'node_modules/vue-multiselect/dist/vue-multiselect.min.js',
-        'node_modules/flatpickr/dist/flatpickr.js',
-    ],
-
-    // Include any special characters you're using in this regular expression
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-
-    whitelistPatterns: [
-        // vue transition classes: https://vuejs.org/v2/guide/transitions.html#Transition-Classes
-        /-enter/,
-        /-leave/,
-    ],
-});
-
 module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
-        ...process.env.NODE_ENV === 'production' ? [purgecss] : [],
     ],
     sourceMap : 'prev',
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 module.exports = {
+    purge: {
+        content: [
+            './html/**/*.php',
+            './html/perch/templates/**/*.html',
+            './html/perch/templates/**/*.php',
+            './html/perch/addons/**/*.html',
+            './html/perch/addons/**/*.php',
+            './html/js/src/**/*.vue',
+            './html/js/src/**/*.js',
+            './node_modules/vue-js-modal/dist/index.js',
+            // './node_modules/vue-multiselect/dist/vue-multiselect.min.js',
+            './node_modules/flatpickr/dist/flatpickr.js',
+        ],
+        options: {
+            whitelist: [
+                // vue transition classes: https://vuejs.org/v2/guide/transitions.html#Transition-Classes
+                /-enter/,
+                /-leave/,
+            ],
+        },
+    },
     theme: {
         extend: {
             colors: {
                 'inherit': 'inherit',
+                'green-oakpark': '#99a23d',
             },
             borderWidth: {
                 '10': '10px',
